28f0f1eab634982064c6009b727067a66bd321a0
Sven Steinbauer <sven@unlogic.co.uk>
2025-07-21 16:57:37 +0100

    Add jump animation (lesson 4):

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,27 +2,30 @@ extends CharacterBody2D
 
 @onready var animation_player_lower: AnimationPlayer = $AnimationPlayerLower
 @onready var animation_player_upper: AnimationPlayer = $AnimationPlayerUpper
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@onready var anchor: Node2D = $anchor
 
 
 func _physics_process(delta: float) -> void:
-	var x_input = Input.get_axis(&"ui_left", &"ui_right")	
-	velocity.x = x_input * 80
+	var x_input = Input.get_axis(&"ui_left", &"ui_right")
+	
+	if not is_on_floor():
+		velocity.y += 500 * delta 
 
+	velocity.x = x_input * 80
+	
+	if Input.is_action_just_pressed(&"ui_up"):
+		velocity.y = -200
+	
 	if x_input == 0:
 		animation_player_lower.play(&"stand")
 		animation_player_upper.play(&"stand")
 	else:
+		anchor.scale.x = sign(x_input)
 		animation_player_lower.play(&"run")
 		animation_player_upper.play(&"run")
+
+	if not is_on_floor():
+		animation_player_lower.play(&"jump")
+		animation_player_upper.play(&"jump")
 
 	move_and_slide()
