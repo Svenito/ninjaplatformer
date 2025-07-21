a72f5bb5f5385649b74427581389c36d0a338603
Sven Steinbauer <sven@unlogic.co.uk>
2025-07-21 20:33:33 +0100

    Add attack animation and sync upper lower (lesson 5)

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,18 @@ extends CharacterBody2D
 @onready var anchor: Node2D = $anchor
 
 
+func _ready() -> void:
+	animation_player_lower.current_animation_changed.connect(func(name: String):
+		if animation_player_upper.current_animation == "attack": return
+		animation_player_upper.play(name)
+	)
+	
+	animation_player_upper.animation_finished.connect(func(name: String):
+		if name != "attack": return
+		animation_player_upper.play(animation_player_lower.current_animation)
+		animation_player_upper.seek(animation_player_lower.current_animation_position)
+	)
+
 func _physics_process(delta: float) -> void:
 	var x_input = Input.get_axis(&"ui_left", &"ui_right")
 	
@@ -13,19 +25,19 @@ func _physics_process(delta: float) -> void:
 
 	velocity.x = x_input * 80
 	
-	if Input.is_action_just_pressed(&"ui_up"):
+	if Input.is_action_just_pressed(&"ui_up") and is_on_floor():
 		velocity.y = -200
+		
+	if Input.is_action_just_pressed(&"attack"):
+		animation_player_upper.play(&"attack")
 	
 	if x_input == 0:
 		animation_player_lower.play(&"stand")
-		animation_player_upper.play(&"stand")
 	else:
 		anchor.scale.x = sign(x_input)
 		animation_player_lower.play(&"run")
-		animation_player_upper.play(&"run")
 
 	if not is_on_floor():
 		animation_player_lower.play(&"jump")
-		animation_player_upper.play(&"jump")
 
 	move_and_slide()
