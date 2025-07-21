7102e26fb0957cd67fecc55208ebf7a427a68b6e
Sven Steinbauer <sven@unlogic.co.uk>
2025-07-21 16:36:50 +0100

    Add player anim (lesson 3)

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+@onready var animation_player_lower: AnimationPlayer = $AnimationPlayerLower
+@onready var animation_player_upper: AnimationPlayer = $AnimationPlayerUpper
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,8 +15,14 @@ func _process(delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	var x_input = Input.get_axis(&"ui_left", &"ui_right")
-	
-	velocity.x = x_input * 50
-	
+	var x_input = Input.get_axis(&"ui_left", &"ui_right")	
+	velocity.x = x_input * 80
+
+	if x_input == 0:
+		animation_player_lower.play(&"stand")
+		animation_player_upper.play(&"stand")
+	else:
+		animation_player_lower.play(&"run")
+		animation_player_upper.play(&"run")
+
 	move_and_slide()
