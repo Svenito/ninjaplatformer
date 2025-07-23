e7a60d0b3558d255b69115fc961f59771f1b8f00
Sven Steinbauer <sven@unlogic.co.uk>
2025-07-23 15:00:12 +0100

    Add sparks to hit (lesson 15)

--- scripts/enemy_cannon.gd
+++ scripts/enemy_cannon.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const SPARK_PARTICLE_BURST_EFFECT = preload("res://scenes/sparks_particle_burst.tscn")
+
 @export var stats: Stats :
 	set(value):
 		stats = value
@@ -15,8 +17,11 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hurtbox.hurt.connect(func(other: Hitbox):
+		var spark_effect = SPARK_PARTICLE_BURST_EFFECT.instantiate()
+		get_tree().current_scene.add_child(spark_effect)
+		spark_effect.global_position = sprite_2d.global_position
 		effects_animation_player.play(&"hit_flash")
 		stats.health -= other.damage
-		shaker.shake(2, 0.2)
+		shaker.shake(2, 0.2)		
 	)
 	stats.no_health.connect(func(): queue_free())
